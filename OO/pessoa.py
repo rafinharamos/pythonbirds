53b11461d298d723e78b6342c9dae94f357f955b
Rafael Ramos <rafaelpr.consultor@ig.com.br>
2018-12-20 16:49:37 -0200

    Criado atributo complexo filhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos,nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola mundo {id(self)}'
 
 if __name__ == "__main__":
     p = Pessoa()
+    Rafael = Pessoa('vicente','Roberto', nome='Rafael')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
     print(p.nome)
-    p.nome = 'Vicente'
+    p.nome = 'Cintia'
     print(p.nome)
+    for filhos in Rafael.filhos:
+        print(filhos)
