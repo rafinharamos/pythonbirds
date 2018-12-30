03f9910bffa1c9bf8a4efbbb184450c2dc33c62a
rafael <rafaelpr.consultor.rr@gmail.com>
2018-12-30 13:08:15 -0200

    Criado e removido atributo dinamico do tipo pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,5 +1,7 @@
 class Pessoa:
-    def __init__(self, *filhos,nome=None, idade=35):
+    olhos = 2
+
+    def __init__(self, *filhos,nome=None, idade=36):
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)
@@ -10,15 +12,19 @@ class Pessoa:
 if __name__ == "__main__":
     p = Pessoa()
     rafael = Pessoa('vicente','Roberto', nome='Rafael')
+    cintia = Pessoa('vicente', 'roberto', nome='Cíntia', idade=39)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
     print(p.nome)
     p.nome = 'Cintia'
+    rafael.olhos = 1
     print(p.nome)
     for filhos in rafael.filhos:
      print(filhos)
     del rafael.filhos
     rafael.sobrenome = "Ramos"
     print(rafael.__dict__)
+    print(cintia.__dict__)
+    print(id(Pessoa.olhos), id(rafael.olhos), id(cintia.olhos))
 
